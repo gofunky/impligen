cc06027a06dd72e6a84714c1e81855f34efc7cf0
matfax <matthias.fax@gmail.com>
2018-07-22 21:12:05 +0200

    Completed interface specification

--- generator.go
+++ generator.go
@@ -1,5 +1,12 @@
 package impligen
 
+const (
+	// If the repository is not public or doesn't meet the requirements although required as such from the generator
+	ErrNotFound = 404
+	// If the code generation fails
+	ErrInternalServerError = 500
+)
+
 type (
 	// The generation parameters provided
 	Params struct {
@@ -11,13 +18,28 @@ type (
 		Type string
 		// The suffix as an optional parameter, "" for the root
 		Suffix string
+		// Target path of an existing git repository where the files are to be generated
+		Target string
 	}
 
+	// The result of the code generation
 	GenResult struct {
-		// TODO TBD how the result code is published
+		// If the generation fails or the parameters don't meet the requirements, otherwise nil
+		Err error
 	}
 
+	// The generator-specific settings for impligen.
+	Settings struct {
+		// If only publically available repositories are allowed as generation argument
+		OnlyPublished bool
+	}
+
+	// To be implemented by the generator.
 	Genner interface {
+		// Get the parameters from the generator
+		Settings() Settings
+
+		// Generate the code now
 		Gen(params Params) GenResult
 	}
 )
